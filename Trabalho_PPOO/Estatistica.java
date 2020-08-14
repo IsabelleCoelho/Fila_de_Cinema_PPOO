8e715360947f7bcf81252628db0aea93ca577f99
RenatoPereira2600 <renatopereira2600@gmail.com>
2020-08-13 23:00:31 -0300

    Javadoc

--- Trabalho_PPOO/Estatistica.java
+++ Trabalho_PPOO/Estatistica.java
@@ -8,11 +8,16 @@ import java.util.Set;
 public class Estatistica {
     private static HashMap<Funcionario, Double> estatisticaFuncionario;
     private static Estatistica instanciaUnica;
-
+    /**
+     * Construtor da classe Estatistica
+     */
     private Estatistica(){
         estatisticaFuncionario = new HashMap<Funcionario, Double>();
     }
-
+    /**
+     * Método responsável por verificar a unicidade do objeto da classe Estatistica
+     * @return Estatistica - Valor de instanciaUnica
+     */
     public static Estatistica getInstance(){
         System.out.println("antes do for");
         if(instanciaUnica == null){
@@ -20,7 +25,11 @@ public class Estatistica {
         }
         return instanciaUnica;
     }
-
+    /**
+     * Método responsável por calcular a média dos tempos de atendimento de um funcionario
+     * @param funcionario - Objeto do tipo Funcionario
+     * @return double - média calculada
+     */
     public double calcularMedia(Funcionario funcionario){
         double mediaAtendimentos = 0;
         ArrayList<Integer> tempos = funcionario.getTempoAtendimentos();
@@ -30,16 +39,25 @@ public class Estatistica {
         }
         return (mediaAtendimentos/(funcionario.getPessoasAtendidas()));
     }
-
+    /**
+     * Método responsável por adicionar um funcionario ao Hashmap estatisticaFuncionario
+     * @param funcionario - Objeto do tipo Funcionario
+     */
     public void adicionarFuncionario(Funcionario funcionario){
         double media = calcularMedia(funcionario);
         estatisticaFuncionario.put(funcionario, media);
     }
-
+    /**
+     * Método responsável por retornar o Hashmap estatisticaFuncionario
+     * @return HashMap<Funcionario, Double> - Hashmap estatisticaFuncionario  
+     */
     public HashMap<Funcionario, Double> getEstatisticaFuncionario(){
         return estatisticaFuncionario;
     }
-
+    /**
+     * Método responsável por calcular a média geral dos tempos de atendimento
+     * @return double - média geral dos tempos de atendimento
+     */
     public double mediaGeral(){
         double medias = 0;
         Set<Funcionario> funcionarios = estatisticaFuncionario.keySet();
